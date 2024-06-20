b1dac68c559a5c3c0bddf92cf112511082833f67
cbeatenbo <97572490+cbeatenbo@users.noreply.github.com>
2024-06-19 21:53:21 -0400

    Update Mortgage_Calculator.js

--- Mortgage_Calculator.js
+++ Mortgage_Calculator.js
@@ -2,15 +2,14 @@ const pBox = document.getElementById("p");
 const rBox = document.getElementById("r");
 const nBox = document.getElementById("n");
 
+
 const answerSpan = document.querySelector("span");
+
 
 const calcMortgage = function(){
     const pValue = Number(pBox.value);
-    const rValue = Number(rBox.value) / 12 / 100;
-    const nValue = Number(nBox.value) * 12;
+    const rValue = Number(rBox.value);
+    const nValue = Number(nBox.value);
     const payment = (pValue * rValue) / (1 - Math.pow(1 + rValue, -nValue));
-    const monthlyPaymentElement = document.querySelector(".monthly_payment");
-    monthlyPaymentElement.textContent = payment.toFixed(2);  
+    answerSpan.textContent = "" + payment.toFixed(2); 
 }
-
-document.getElementById("calculateButton").addEventListener("click", calcMortgage);
